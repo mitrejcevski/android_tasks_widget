f5135c96a947b99e211449e1ee89ac9befa61df7
Jovche Mitrejchevski <jovche.mitrejchevski@ask.fm>
2016-02-18 16:41:13 +0200

    Code formatting

--- TasksWidget/widget/src/main/java/com/mitrejcevski/widget/model/Group.java
+++ TasksWidget/widget/src/main/java/com/mitrejcevski/widget/model/Group.java
@@ -2,42 +2,42 @@ package com.mitrejcevski.widget.model;
 
 /**
  * Class used for representation for the groups.
- * 
+ *
  * @author jovche.mitrejchevski
  */
 public class Group {
 
-	private int mId;
-	private String mGroupTitle;
-	private boolean mShouldDelete;
+    private int mId;
+    private String mGroupTitle;
+    private boolean mShouldDelete;
 
-	/* Getters and Setters */
-	public int getId() {
-		return mId;
-	}
+    /* Getters and Setters */
+    public int getId() {
+        return mId;
+    }
 
-	public void setId(int id) {
-		mId = id;
-	}
+    public void setId(int id) {
+        mId = id;
+    }
 
-	public String getGroupTitle() {
-		return mGroupTitle;
-	}
+    public String getGroupTitle() {
+        return mGroupTitle;
+    }
 
-	public void setGroupTitle(String groupTitle) {
-		mGroupTitle = groupTitle;
-	}
+    public void setGroupTitle(String groupTitle) {
+        mGroupTitle = groupTitle;
+    }
 
-	public void setShoudlDelete(boolean shoudlDelete) {
-		mShouldDelete = shoudlDelete;
-	}
+    public void setShoudlDelete(boolean shoudlDelete) {
+        mShouldDelete = shoudlDelete;
+    }
 
-	public boolean shouldDelete() {
-		return mShouldDelete;
-	}
+    public boolean shouldDelete() {
+        return mShouldDelete;
+    }
 
-	@Override
-	public String toString() {
-		return mGroupTitle;
-	}
+    @Override
+    public String toString() {
+        return mGroupTitle;
+    }
 }
--- TasksWidget/widget/src/main/java/com/mitrejcevski/widget/provider/MyWidgetService.java
+++ TasksWidget/widget/src/main/java/com/mitrejcevski/widget/provider/MyWidgetService.java
@@ -5,12 +5,11 @@ import android.widget.RemoteViewsService;
 
 /**
  * Service for binding the remote views.
- * 
+ *
  * @author jovche.mitrejchevski
- * 
  */
 public class MyWidgetService extends RemoteViewsService {
-	public RemoteViewsFactory onGetViewFactory(Intent intent) {
-		return new MyWidgetFactory(this.getApplicationContext(), intent);
-	}
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new MyWidgetFactory(this.getApplicationContext(), intent);
+    }
 }
