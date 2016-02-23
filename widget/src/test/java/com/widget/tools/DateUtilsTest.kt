c86a15a8e64c684841384bc02b48542f9d6d1990
Jovche Mitrejchevski <jovche.mitrejchevski@ask.fm>
2016-02-23 11:31:36 +0200

    Rename utility class

--- widget/src/test/java/com/widget/tools/DateUtilsTest.kt
+++ widget/src/test/java/com/widget/tools/DateUtilsTest.kt
@@ -1,19 +1,19 @@
 package com.widget.tools
 
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class TestUtilsTest {
+class DateUtilsTest {
 
     @Test
     fun dateUtilsShouldProperlyFormatDate() {
         val dateTime = 1451606400000;
-        Assert.assertEquals("01/01/2016", convertToDateFormat(dateTime))
+        assertEquals("01/01/2016", convertToDateFormat(dateTime))
     }
 
     @Test
     fun dateUtilsShouldProperlyFormatDateWithTime() {
         val dateTime = 1451606400000;
-        Assert.assertEquals("01/01/2016 02:00", convertToDateTimeFormat(dateTime))
+        assertEquals("01/01/2016 02:00", convertToDateTimeFormat(dateTime))
     }
 }
